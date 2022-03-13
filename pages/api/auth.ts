a40a95b18dec5a6ec8411775a5aa4a91715a8b5f
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-13 12:57:03 +0700

    extract params from request as json then return username back

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -2,13 +2,14 @@
 import type { NextApiRequest, NextApiResponse} from "next"
 import type { AuthResult } from '../types/auth-result'
 
-
 export default function handler( 
     req: NextApiRequest,
     res: NextApiResponse<AuthResult>
 ) {
+    const { username, password } = req.body
+
     res.status(200).json({ 
         result: true, 
-        message: 'Hello World' 
+        message: `Hello ${username}`
     })
 }
